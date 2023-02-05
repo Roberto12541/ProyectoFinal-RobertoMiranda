d51d17594f26985121afee18a82fee4168c9ccd3
Roberto12541 <robertomiranda1254@gmail.com>
2023-02-05 17:08:06 -0600

    Vistas terminadas

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ document.getElementById("icon_menu").addEventListener("click", mostrar_menu);
 
 function mostrar_menu(){
 
-    document.querySelector(".menu").classList.toggle("mostrar_menu");
+    document.querySelector(".menu").classList.toggle("menu-extend");
     
 }
 
